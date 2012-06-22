41e7cec9c3f2a843edfbde779a3372949012ddfa
Frederik Dietz <fdietz@gmail.com>
2012-06-22 15:13:16 +0200

    cleanup counter widget duplication

--- app/assets/javascripts/views/widgets/counter.js
+++ app/assets/javascripts/views/widgets/counter.js
@@ -1,6 +1,82 @@
 (function ($, _, Backbone, views, models, collections){
   "use strict";
 
+  var CounterSubview = Backbone.View.extend({
+
+    className: 'counter',
+
+    initialize: function(options) {
+      _.bindAll(this, "render");
+
+      this.secondaryModel = options.secondaryModel;
+    },
+
+    value: function() {
+      return this.model.get('value') || 0;
+    },
+
+    secondaryValue: function() {
+      var y1 = this.model.get('value') || 0;
+      var y2 = this.secondaryModel.get('value') || 0;
+      if (y1 && y2) {
+        var result = ((y1 - y2) / y2) * 100;
+        return result.toFixed(2);
+      } else {
+        return 0;
+      }
+    },
+
+    render: function() {
+      var value = this.value();
+      var secondaryValue = this.secondaryValue();
+
+      $(this.el).html(JST['templates/widgets/counter/subview']({ 
+        value: Math.abs(value),
+        secondaryValue: Math.abs(secondaryValue)
+      }));
+
+      this.$value = this.$('.value');
+      this.$secondaryValue = this.$('.secondary-value');
+      this.$arrow = this.$('.arrow');
+      this.$rateContainer = this.$('.rate-container');
+
+      this.updateValueSizeClass(value);
+      this.updateSecondaryValueClass(secondaryValue);
+
+      return this;
+    },
+
+    updateSecondaryValueClass: function(secondaryValue) {
+      if (secondaryValue > 0) {
+        this.$arrow.removeClass('arrow-down');
+        this.$arrow.addClass('arrow-up');
+        this.$rateContainer.addClass('secondary-value-up');
+        this.$rateContainer.removeClass('secondary-value-down');
+      } else {
+        this.$arrow.addClass('arrow-down');
+        this.$arrow.removeClass('arrow-up');
+        this.$rateContainer.addClass('secondary-value-down');
+        this.$rateContainer.removeClass('secondary-value-up');
+      }
+    },
+
+    updateValueSizeClass: function(value) {
+      var str = value.toString().length;
+      this.$value.removeClass("value-size-large");
+      this.$value.removeClass("value-size-medium");
+      this.$value.removeClass("value-size-small");
+
+      if (str <= 5) {
+        this.$value.addClass("value-size-large");
+      } else if (str > 5 && str < 8) {
+        this.$value.addClass("value-size-medium");
+      } else {
+        this.$value.addClass("value-size-small");
+      }
+    }
+
+  });
+
   views.widgets.Counter = Backbone.View.extend({
 
     initialize: function(options) {
@@ -58,91 +134,13 @@
       this.render();
     },
 
-    value: function() {
-      return this.counterModel.get('value') || 0;
-    },
-
-    secondaryValue: function() {
-      var y1 = this.counterModel.get('value') || 0;
-      var y2 = this.secondaryCounterModel.get('value') || 0;
-      if (y1 && y2) {
-        var result = ((y1 - y2) / y2) * 100;
-        return result.toFixed(2);
-      } else {
-        return 0;
-      }
-    },
-
-    value2: function() {
-      return this.counterModel2.get('value') || 0;
-    },
-
-    secondaryValue2: function() {
-      var y1 = this.counterModel2.get('value') || 0;
-      var y2 = this.secondaryCounterModel2.get('value') || 0;
-      if (y1 && y2) {
-        var result = ((y1 - y2) / y2) * 100;
-        return result.toFixed(2);
-      } else {
-        return 0;
-      }
-    },
-
-    updateSecondaryValueClass: function(element, arrowElement, secondaryValue) {
-      if (secondaryValue > 0) {
-        arrowElement.removeClass('arrow-down');
-        arrowElement.addClass('arrow-up');
-        element.addClass('secondary-value-up');
-        element.removeClass('secondary-value-down');
-      } else {
-        arrowElement.addClass('arrow-down');
-        arrowElement.removeClass('arrow-up');
-        element.addClass('secondary-value-down');
-        element.removeClass('secondary-value-up');
-      }
-    },
-
-    updateValueSizeClass: function(element, value) {
-      var str = value.toString().length;
-      element.removeClass("value-size-large");
-      element.removeClass("value-size-medium");
-      element.removeClass("value-size-small");
-
-      if (str <= 5) {
-        element.addClass("value-size-large");
-      } else if (str > 5 && str < 8) {
-        element.addClass("value-size-medium");
-      } else {
-        element.addClass("value-size-small");
-      }
-    },
-
     render: function() {
-      var value = this.value();
-      var secondaryValue = this.secondaryValue();
-      var value2 = this.value2();
-      var secondaryValue2 = this.secondaryValue2();
+      this.counter1 = new CounterSubview({ model: this.counterModel, secondaryModel: this.secondaryCounterModel });
+      this.counter2 = new CounterSubview({ model: this.counterModel2, secondaryModel: this.secondaryCounterModel2 });
 
-      $(this.el).html(JST['templates/widgets/counter/show']({
-        value: value,
-        secondaryValue: Math.abs(secondaryValue),
-        value2: value2,
-        secondaryValue2: Math.abs(secondaryValue2)
-      }));
-
-      this.$value = this.$('.counter .value');
-      this.$value2 = this.$('.counter2 .value');
-      this.$secondaryValue = this.$('.counter .secondary-value');
-      this.$secondaryValue2 = this.$('.counter2 .secondary-value');
-      this.$arrow = this.$('.counter .arrow');
-      this.$arrow2 = this.$('.counter2 .arrow');
-      this.$rateContainer = this.$('.counter .rate-container');
-      this.$rateContainer2 = this.$('.counter2 .rate-container');
-
-      this.updateValueSizeClass(this.$value, value);
-      this.updateValueSizeClass(this.$value2, value2);
-      this.updateSecondaryValueClass(this.$rateContainer, this.$arrow, secondaryValue);
-      this.updateSecondaryValueClass(this.$rateContainer2, this.$arrow2, secondaryValue2);
+      $(this.el).empty();
+      $(this.el).append(this.counter1.render().el);
+      $(this.el).append(this.counter2.render().el);
 
       return this;
     },
