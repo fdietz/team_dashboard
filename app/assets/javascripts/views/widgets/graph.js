53c25e0bbb01e24bee24250c6860b02723559a36
Frederik Dietz <fdietz@gmail.com>
2012-07-21 15:29:16 +0200

    fix time formatting for hover detail

--- app/assets/javascripts/views/widgets/graph.js
+++ app/assets/javascripts/views/widgets/graph.js
@@ -117,7 +117,7 @@
       var hoverDetail = new Rickshaw.Graph.HoverDetail({
         graph: this.graph,
         formatter: function(series, x, y) {
-          var dateStr = window.moment.utc(new Date(x*1000)).local().format("YYYY-MM-DD h:mm a");
+          var dateStr = window.moment.utc(new Date(x*1000)).local().format("YYYY-MM-DD HH:mm");
           var date = '<span class="date">' + dateStr + '</span>';
           var swatch = '<span class="detail_swatch" style="background-color: ' + series.color + '"></span>';
           var content = swatch + series.name + ": " + parseInt(y) + '<br>' + date;
