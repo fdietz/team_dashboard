e099beb117f4c2740bea5ff081ba08e29cd15ec1
Raffael Schmid <raffael@yux.ch>
2012-08-09 22:04:25 +0200

    use faraday/http_proxy to request the jenkins data

--- app/models/sources/ci/jenkins.rb
+++ app/models/sources/ci/jenkins.rb
@@ -16,10 +16,9 @@ module Sources
       end
 
       def request_build_status(server_url, project)
-        request_url = "#{server_url}/job/#{project}/lastBuild/api/json"
-        uri = URI.parse(request_url)
-        Rails.logger.debug("Requesting from #{uri} ...")
-        JSON.parse(uri.read)
+        url = "#{server_url}/job/#{project}/lastBuild/api/json"
+        Rails.logger.debug("Requesting from #{url} ...")
+        ::HttpProxy.request(url)
       end
 
       def status(status)
