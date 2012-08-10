59b13644cd546ae3efccef460e219f247cd59753
Frederik Dietz <frederik.dietz@xing.com>
2012-08-10 11:19:40 +0200

    some minor modifications

--- app/models/http_proxy.rb
+++ app/models/http_proxy.rb
@@ -1,20 +1,20 @@
-require 'faraday'
-require 'faraday_middleware'
-
 module HttpProxy
   extend self
 
   def request(url)
     uri = URI.parse(url)
-    connection = Faraday.new(url: "#{uri.scheme}://#{uri.host}:#{uri.port}") do |conn|
-      conn.request :json
+    connection = Faraday.new(uri) do |conn|
+      conn.use FaradayMiddleware::ParseJson,       content_type: 'application/json'
+      conn.use FaradayMiddleware::FollowRedirects, limit: 3
+      conn.use Faraday::Response::RaiseError      
+
+      conn.request  :json
       conn.response :json, :content_type => /\bjson$/
-      conn.basic_auth(uri.user, uri.password) if uri.user or uri.password
+      
+      conn.basic_auth(uri.user, uri.password) if uri.user && uri.password
+      
       conn.adapter Faraday.default_adapter
     end
-    connection.get("#{uri.path}?#{uri.query}").body
-  rescue => e
-    Rails.logger.error "Error while parsing JSON response: #{e}"
-    nil
+    connection.get(url).body
   end
 end
