5318c46d1d7ccad8b5be26332b694fe6cdd4b412
Frederik Dietz <fdietz@gmail.com>
2013-03-09 17:15:05 +0100

    create script tag for all inline templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,35 @@ module ApplicationHelper
 		Time.at(timestamp)
 	end
 
-  # TODO: cleanup all three methods!!
+  # TODO: cleanup all!!
 
+  # Performance optimization, since we can't precompile Angular.js templates
+  # but don't want to use inline templates, we create <script> tags instead
+  #
+  # When "fetching" templates based on the URL Angular.js will first check if
+  # a script tag with an ID matching a template URL exists before doing the
+  # HTTP request.
+  #
+  # It would be nice to use sprockets JST to load and compile templates
+  # client-side instead in the future.
+  #
   def script_tag_for_all_templates
+    templates_dir = Rails.root.join("app/assets/javascripts/templates")
+    files = Dir[templates_dir + "**/*.html"].reject {|fn| File.directory?(fn) }
+    files += Dir[templates_dir + "**/*.html.erb"].reject {|fn| File.directory?(fn) }
+
+    puts files
+    files.map do |file|
+      id = "/assets/templates/#{Pathname(file).relative_path_from(templates_dir)}".gsub(".erb", "")
+      <<-EOF
+      <script type="text/ng-template" id="#{id}">
+        #{render :file => file, :formats => [:html]}
+      </script>
+      EOF
+    end.join("\n").html_safe
+  end
+
+  def script_tag_for_all_custom_fields
     Widget.list_available.map do |widget|
       id = "templates/custom_fields/#{widget}"
       <<-EOF
