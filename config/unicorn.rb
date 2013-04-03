6bfe471a296f0461ca1e25e9c5b36fe3d9676821
Frederik Dietz <fdietz@gmail.com>
2013-04-03 22:41:21 +0200

    try different unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,16 +4,15 @@ timeout 30
 listen 3000
 
 after_fork do |server, worker|
-	ActiveRecord::Base.establish_connection
+	if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
 end
 
 before_fork do |server, worker|
-  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
   end
 end
