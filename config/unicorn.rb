18c6cee22ed2b2b90edad0e2e6c21faf0818e776
Frederik Dietz <fdietz@gmail.com>
2013-04-06 12:22:59 +0200

    another try heroku unicornÄ

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,18 +1,30 @@
-worker_processes 4
+# based on https://devcenter.heroku.com/articles/rails-unicorn
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 preload_app true
 timeout 30
-listen 3000
+listen Integer(ENV["PORT"] || 3000)
 
 after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
 	if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
+    puts 'Connected to ActiveRecord'
   end
 end
 
 before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
+    puts 'Disconnected from ActiveRecord'
   end
 end
