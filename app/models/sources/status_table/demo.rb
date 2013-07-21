646cfbf0253b50b8c07c189eedfcd18767be020e
Paul Hamby <phamby@gmail.com>
2013-07-21 01:45:39 +0000

    adding row level status to status_table widget

--- app/models/sources/status_table/demo.rb
+++ app/models/sources/status_table/demo.rb
@@ -20,19 +20,23 @@ module Sources
         all_messages = Array.new
         for i in 0..max-1
           values_array.push(parsed_json[i]["status"])
-          all_messages << {:label => "#{parsed_json[i]["label"]}", :value => "#{parsed_json[i]["value"]}"}
+          all_messages << {
+            :status => "#{parsed_json[i]["status"]}",
+            :label => "#{parsed_json[i]["label"]}",
+            :value => "#{parsed_json[i]["value"]}"
+          }
         end
 
         if !values_array.empty?
           overall_value = case
-          when values_array.include?(2)
-            2
-          when values_array.include?(1)
-            1
-          when values_array.include?(0)
-            0
-          else
-            Rails.logger.debug("Error getting status from file")
+            when values_array.include?(2)
+              2
+            when values_array.include?(1)
+              1
+            when values_array.include?(0)
+              0
+            else
+              Rails.logger.debug("Error getting status from file")
           end
         else
           overall_value = 0
