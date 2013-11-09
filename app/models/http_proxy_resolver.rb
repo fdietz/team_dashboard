324aa8b4163b5218af9ea380903e94536c7b4bd5
Frederik Dietz <fdietz@gmail.com>
2013-11-09 13:12:08 +0100

    add more logging to asset debugging http_proxy configuration

--- app/models/http_proxy_resolver.rb
+++ app/models/http_proxy_resolver.rb
@@ -9,12 +9,16 @@ module HttpProxyResolver
 
   def get(options = {})
     widget        = Widget.find(options.fetch(:widget_id))
-    response_body = ::HttpService.request(widget.settings.fetch(:proxy_url), :headers => { :accept =>  'application/json' })
     value_path    = widget.settings.fetch(:proxy_value_path);
+    response_body = ::HttpService.request(widget.settings.fetch(:proxy_url), :headers => { :accept =>  'application/json' })
+
+    Rails.logger.debug "HttpProxyResolver - Response Body: #{response_body}"
+    if value_path.present?
+      Rails.logger.debug "HttpProxyResolver - value_path: #{value_path} - resolved value: #{resolve_value(response_body, value_path)}"
+    end
 
     if value_path.present?
-      result = { :value => resolve_value(response_body, value_path) }
-      result
+      { :value => resolve_value(response_body, value_path) }
     else
       response_body
     end
