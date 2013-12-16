79ef270341eb7229e3573b6ddbc261465111fda2
Daniel Hahn <dha@betterplace.org>
2013-12-16 12:36:14 +0100

    Fix jenkins status table

--- app/models/sources/status_table/jenkins.rb
+++ app/models/sources/status_table/jenkins.rb
@@ -1,10 +1,11 @@
 module Sources
   module StatusTable
-    class Pingdom < Sources::StatusTable::Base
+    class Jenkins < Sources::StatusTable::Base
 
       def custom_fields
         [
-          { :name => "server_url", :title => "Server Url", :mandatory => true }
+          { name: "server_url", title: "Server Url", mandatory: true },
+          { name: "projects", title: "Projects (regexp)", mandatory: false }
         ]
       end
 
@@ -19,7 +20,15 @@ module Sources
       def get(options = {})
         widget     = Widget.find(options.fetch(:widget_id))
         server_url = widget.settings.fetch(:server_url)
-        jenkins(server_url).status_table
+        projects = widget.settings[:projects]
+        status_table = filter_status(projects, jenkins(server_url).status_table)
+
+        build_json_response(status_table)
+      end
+
+      def filter_status(projects, status_table)
+        return status_table if projects.blank?
+        status_table.reject { |stat| !(stat['label'] =~ /#{projects}/) }
       end
 
     end
